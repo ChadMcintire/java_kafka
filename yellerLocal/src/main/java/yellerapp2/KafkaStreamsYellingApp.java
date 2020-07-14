12a8f4f774457db5cfd4a0293eb43f8089a883d2
ChadMcIntire <chadmcintirecloud@gmail.com>
2020-07-14 17:41:03 -0600

    first working incarnation of a simple kafka stream app

---
+++ yellerLocal/src/main/java/yellerapp2/KafkaStreamsYellingApp.java
@@ -0,0 +1,48 @@
+package yellerapp2;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.Consumed;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Properties;
+
+
+public class KafkaStreamsYellingApp {
+
+ public static void main(String[] args) throws Exception {
+
+  Properties props = new Properties();
+
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "yelling_app_id");   
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+  StreamsConfig streamsConfig = new StreamsConfig(props);
+
+  Serde<String> stringSerde = Serdes.String();
+
+  StreamsBuilder builder = new StreamsBuilder();
+
+  KStream<String, String> simpleFirstStream = builder.stream("text-input",
+    Consumed.with(stringSerde, stringSerde));
+
+  KStream<String, String> upperCasedStream =
+    simpleFirstStream.mapValues(String::toUpperCase);
+
+  upperCasedStream.to( "text-output",Produced.with(stringSerde, stringSerde)); 
+
+  KafkaStreams kafkaStreams = new KafkaStreams(builder.build(),streamsConfig);
+
+
+  kafkaStreams.start();
+
+  Thread.sleep(120000);
+  kafkaStreams.close();
+
+  }
+}
